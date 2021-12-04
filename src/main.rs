bc10dc23386058a14db8a296065c049dd5e3505b
df@ <@>
2021-12-04 22:44:53 +0000

    ok

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,37 @@
 struct Job {
     frames: i32,
+    done: bool,
 }
 
 impl Job {
     fn new() -> Self {
-        Self { frames: 10 }
+        Self {
+            frames: 10,
+            done: false,
+        }
     }
 
     fn render(&mut self) {
-        self.frames -= 1;
+        if !self.get_done() {
+            self.frames -= 1;
+        }
+        if self.frames == 0 {
+            self.done = true
+        }
+    }
+
+    fn get_done(&self) -> bool {
+        self.done
     }
 }
 
 
 fn main() {
     let mut job = Job::new();
-    job.render();
+    let mut cycle = 0;
+    while !job.get_done() {
+        println!("Cycle: {}", cycle);
+        job.render();
+        cycle += 1;
+    }
 }
