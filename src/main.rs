2c257443a2d62cc2b08238fb31baac4272a3e1c8
df@ <@>
2021-12-04 22:39:39 +0000

    ok

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
+struct Job {
+    frames: i32,
+}
 
+impl Job {
+    fn new() -> Self {
+        Self { frames: 10 }
+    }
+
+    fn render(&mut self) {
+        self.frames -= 1;
+    }
+}
+
+
+fn main() {
+    let mut job = Job::new();
+    job.render();
 }
