0dcd728d8e31422315cd386904dffd967354fdf2
df@ <@>
2021-12-04 01:46:14 +0000

    removing extra; dual line plot

--- src/main.rs
+++ src/main.rs
@@ -3,21 +3,25 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let root = BitMapBackend::new("plotters-doc-data/0.png", (1280, 720)).into_drawing_area();
     root.fill(&WHITE)?;
     let mut chart = ChartBuilder::on(&root)
-        .caption("y=x^2", ("sans-serif", 50).into_font())
         .margin(5)
         .x_label_area_size(30)
         .y_label_area_size(30)
         .build_cartesian_2d(-1f32..1f32, -0.1f32..1f32)?;
 
-    chart.configure_mesh().draw()?;
+    chart
+        .configure_mesh()
+        .draw()?;
 
     chart
         .draw_series(LineSeries::new(
             (-50..=50).map(|x| x as f32 / 50.0).map(|x| (x, x * x)),
             &RED,
-        ))?
-        .label("y = x^2")
-        .legend(|(x, y)| PathElement::new(vec![(x, y), (x + 20, y)], &RED));
+        ))?;
+    chart
+        .draw_series(LineSeries::new(
+            (-150..=150).map(|x| (x as f32 + 0.2) / 150.0).map(|x| (x, x )),
+            &GREEN,
+        ))?;
 
     Ok(())
 }
