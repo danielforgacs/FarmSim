e863560a37c66d65af2943c5e41ee165aade5229
df@ <@>
2021-12-04 01:33:20 +0000

    removed extra

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use plotters::prelude::*;
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let root = BitMapBackend::new("plotters-doc-data/0.png", (640, 480)).into_drawing_area();
+    let root = BitMapBackend::new("plotters-doc-data/0.png", (1280, 720)).into_drawing_area();
     root.fill(&WHITE)?;
     let mut chart = ChartBuilder::on(&root)
         .caption("y=x^2", ("sans-serif", 50).into_font())
@@ -18,12 +18,6 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         ))?
         .label("y = x^2")
         .legend(|(x, y)| PathElement::new(vec![(x, y), (x + 20, y)], &RED));
-
-    chart
-        .configure_series_labels()
-        .background_style(&WHITE.mix(0.8))
-        .border_style(&BLACK)
-        .draw()?;
 
     Ok(())
 }
