6db99ebccb1bd26c36763321b02c4666f661cbe8
df@ <@>
2021-12-04 01:54:20 +0000

    clean up.

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,6 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         .margin(5)
         .x_label_area_size(25)
         .y_label_area_size(25)
-        // .build_cartesian_2d(-1f32..1f32, -0.1f32..1f32)?;
         .build_cartesian_2d(0_f32..250_f32, 0_f32..100_f32)?;
 
     chart
@@ -15,7 +14,6 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     chart
         .draw_series(LineSeries::new(
-            // (-150..=150).map(|x| (x as f32 + 0.2) / 150.0).map(|x| (x, x )),
             (0..=250).map(|x| x as f32).map(|x| (x, x )),
             &GREEN,
         ))?;
