5e4e1859f8267b58badeed09ca2a029f53cd0094
df@ <@>
2021-12-05 00:55:58 +0000

    farm seemts to be working.

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,46 @@
 struct Job {
+    id: i32,
     frames: i32,
-    done: bool,
+    total_frames: i32,
+    chunk_size: i32,
+    task_count: i32,
 }
 
 struct Farm {
     jobs: Vec<Job>,
+    cpus: i32,
+    free_cpus: i32,
 }
 
 impl Job {
-    fn new() -> Self {
+    fn new(id: i32, frames: i32, chunk_size: i32) -> Self {
+        let mut tasks = frames / chunk_size;
+        if frames % chunk_size > 0 {
+            tasks += 1;
+        }
         Self {
-            frames: 10,
-            done: false,
+            id,
+            frames,
+            total_frames: frames,
+            chunk_size,
+            task_count: tasks,
         }
     }
 
     fn render(&mut self) {
-        if !self.get_done() {
-            println!("{:<22}{:p}", "rendering job:", self);
+        if self.frames > 0 {
             self.frames -= 1;
         }
-        if self.frames == 0 {
-            self.done = true
-        }
-    }
-
-    fn get_done(&self) -> bool {
-        self.done
     }
 }
 
 impl Farm {
-    fn new() -> Self {
-        Self { jobs: Vec::new() }
+    fn new(cpus: i32) -> Self {
+        Self {
+            jobs: Vec::new(),
+            cpus,
+            free_cpus: cpus,
+        }
     }
 
     fn submit(&mut self, job: Job) {
@@ -40,20 +48,36 @@ impl Farm {
     }
 
     fn render(&mut self) {
-        for job in self.jobs.iter_mut() {
-            job.render();
+        'mainloop: for job in self.jobs.iter_mut() {
+            for _ in 0..job.task_count {
+                if self.free_cpus == 0 {
+                    break 'mainloop;
+                }
+                self.free_cpus -= 1;
+                job.render();
+                println!("job id: {}, frames: {}, chunk size: {}, tasks: {}, frames left: {}", job.id, job.total_frames, job.chunk_size, job.task_count, job.frames);
+                if job.frames == 0 {
+                    break;
+                }
+            }
         }
-        self.jobs.retain(|x| !x.get_done());
+        self.jobs.retain(|x| x.frames > 0);
+        self.free_cpus = self.cpus;
     }
 }
 
 fn main() {
-    let job = Job::new();
-    let mut farm = Farm::new();
-    farm.submit(job);
+    let job1 = Job::new(0, 4, 2);
+    let job2 = Job::new(1, 4, 2);
+    let mut farm = Farm::new(4);
+    farm.submit(job1);
+    farm.submit(job2);
     for cycle in 0..=11 {
         println!("--- cycle: {} -------------------", cycle);
-        println!("{:<22}{}", "job count:", &farm.jobs.len());
+        println!(
+            "job count: {}",
+            &farm.jobs.len()
+        );
         farm.render();
     }
 }
